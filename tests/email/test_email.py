636f8f6ff707e7b5a7a114c464a7094c252d41eb
Moasib-Arif <moasib_arif@yahoo.com>
2024-03-20 13:24:07 +0000

    minor changes to the tests

--- tests/email/test_email.py
+++ tests/email/test_email.py
@@ -1,15 +1,16 @@
 import pytest
 import boto3
-from moto import mock_ses
+from moto import mock_aws
 from dpytools.email.ses.client import SesClient
+import botocore
 
 @pytest.fixture
-@mock_ses
+@mock_aws
 def mock_ses_client():
     boto3.setup_default_session(region_name='us-west-2')
     return boto3.client('ses')
 
-@mock_ses
+@mock_aws
 def test_ses_client_initialisation(mock_ses_client):
     """
     Test the initialisation of the SesClient.
@@ -23,25 +24,25 @@ def test_ses_client_initialisation(mock_ses_client):
     # Assert that the client is not None
     assert mock_ses_client.client is not None
 
-@mock_ses
+@mock_aws
 def test_ses_client_initialisation_invalid_sender_email():
     """
     Test the initialisation of the SesClient with an invalid sender email.
     """
     with pytest.raises(ValueError) as e:
         SesClient('invalid_email', 'us-west-2')
-    assert 'Invalid sender email format: invalid_email' in str(e.value)
+    assert 'Invalid sender email format' in str(e.value)
 
-@mock_ses
+@mock_aws
 def test_ses_client_initialisation_invalid_aws_region():
     """
     Test the initialisation of the SesClient with an invalid AWS region.
     """
     with pytest.raises(ValueError) as e:
         SesClient('sender@example.com', 'invalid_region')
-    assert 'Invalid AWS region: invalid_region' in str(e.value)
+    assert 'Invalid AWS region' in str(e.value)
 
-@mock_ses
+@mock_aws
 def test_ses_client_send_invalid_recipient(mock_ses_client):
     """
     Test the send method of the SesClient with an invalid recipient.
@@ -52,9 +53,9 @@ def test_ses_client_send_invalid_recipient(mock_ses_client):
     with pytest.raises(ValueError) as e:
         mock_ses_client.send('invalid_email', 'subject', 'body')
 
-    assert 'Invalid recipient email format: invalid_email' in str(e.value)
+    assert 'Invalid recipient email format' in str(e.value)
 
-@mock_ses
+@mock_aws
 def test_ses_client_send_missing_subject(mock_ses_client):
     """
     Test the send method of the SesClient with a missing subject.
@@ -62,12 +63,10 @@ def test_ses_client_send_missing_subject(mock_ses_client):
     # Initialise the SES client
     mock_ses_client = SesClient('sender@example.com', 'us-west-2')
 
-    with pytest.raises(TypeError) as e:
+    with pytest.raises(botocore.exceptions.ParamValidationError) as e:
         mock_ses_client.send('recipient@example.com', None, 'body')
-    
-    assert 'Missing Subject' in str(e.value)
 
-@mock_ses
+@mock_aws
 def test_ses_client_send_missing_body(mock_ses_client):
     """
     Test the send method of the SesClient with a missing body.
@@ -75,18 +74,19 @@ def test_ses_client_send_missing_body(mock_ses_client):
     # Initialise the SES client
     mock_ses_client = SesClient('sender@example.com', 'us-west-2')
 
-    with pytest.raises(TypeError) as e:
+    with pytest.raises(botocore.exceptions.ParamValidationError) as e:
         mock_ses_client.send('recipient@example.com', 'subject', None)
-        
-    assert 'Missing Body' in str(e.value)
 
-@mock_ses
+@mock_aws
 def test_ses_client_send_email(mock_ses_client):
     """
     Test the send method of the SesClient.
     """
     # Initialise the SES client
     mock_ses_client = SesClient('sender@example.com', 'us-west-2')
+
+    # Verify the sender's email address
+    mock_ses_client.client.verify_email_identity(EmailAddress='sender@example.com')
 
     # Send an email
     response = mock_ses_client.send('recipient@example.com', 'subject', 'body')
