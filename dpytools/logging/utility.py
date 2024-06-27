5195d796d414d8f503cc0ef3393bcee474c868dc
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2024-06-27 08:50:44 +0100

    added logging stuff

--- dpytools/logging/utility.py
+++ dpytools/logging/utility.py
@@ -1,6 +1,10 @@
 import sys
 import traceback
+from requests import Response
+from datetime import datetime, timedelta
 from typing import Dict, List
+
+from urllib.parse import urlparse
 
 
 def level_to_severity(level: int) -> int:
@@ -47,3 +51,72 @@ def create_error_dict(error: Exception) -> List[Dict]:
 
     # Listify in keeping with expected DP logging structures
     return [error_dict]
+
+def get_scheme(url: str) -> str:
+    """This function will return the scheme from the provided url."""
+
+    index = url.find('/')
+    scheme = url[0:index-1]
+    return scheme
+
+def get_domain(url: str) -> str:
+    """This funciton will return the domain name from the provided url."""
+    #Parsing url to extract the domain name
+    parsed_url = urlparse(url)
+
+    #Getting the domain name
+    domain = parsed_url.netloc.split(':')[0]
+    return domain
+
+def get_port(url: str) ->int:
+    """This funciton will return the port number form the provided url."""
+    #Parsing url
+    parsed_url = urlparse(url)
+
+    #checking if the port was give if not scheking scheme for port number
+    if parsed_url.port is None:
+        if parsed_url.scheme == "http":
+            return 80
+        else:
+            return 443
+    else:
+        return parsed_url.port
+
+def get_start_date(date: str) ->str:
+    
+    strp_time = datetime.strptime(date, '%a, %d %b %Y %H:%M:%S GMT')
+    
+    return strp_time.isoformat() + "Z"
+
+def get_end_date(time_delta: timedelta, date: str) ->str:
+    """This function will calculate the end_date by addint the duration to the start date."""
+
+    strp_time = datetime.strptime(date, '%a, %d %b %Y %H:%M:%S GMT')
+    td = timedelta(microseconds=time_delta.microseconds)
+    
+    end_date = strp_time + td
+
+    return end_date.isoformat() + "Z"
+
+def calculate_duration_in_nanoseconds(time_delta: timedelta, date: str)->int:
+    """This funciton will convert the duration from Miliseconds to Nanoseconds."""
+
+    strp_time = datetime.strptime(date, '%a, %d %b %Y %H:%M:%S GMT')
+    td = timedelta(microseconds=time_delta.microseconds)
+    
+    end_date = strp_time + td
+
+    duration = end_date - strp_time
+    
+    return duration.microseconds * 1000 
+
+def get_content_length(res: Response)->int:
+    """
+    This funciton will try to get the 'Content-Lenght'
+    if there is noone it will return a default 0.
+    """
+    try:
+        content_length = res.headers["Content-Length"]
+        return int(content_length)
+    except KeyError:
+        return 0
