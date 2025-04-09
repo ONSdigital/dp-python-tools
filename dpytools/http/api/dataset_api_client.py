7759dc8616c380021ee171add45404997efd8284
Jim Washbrook <jim.washbrook@ext.ons.gov.uk>
2025-04-09 11:40:58 +0100

    feat: raise error + add response body
    fix: return response
    
    add catches around other things
    
    wip: error logging better
    
    better logging
    
    fix: tests
    
    chore: formatting

--- dpytools/http/api/dataset_api_client.py
+++ dpytools/http/api/dataset_api_client.py
@@ -1,6 +1,7 @@
-from typing import Dict, Union
+from typing import Dict, Optional, Union
 
 from requests import Response
+from requests.exceptions import HTTPError
 
 from dpytools.http.base_http import BaseHttpClient
 from dpytools.http.token_auth import TokenAuth
@@ -27,14 +28,16 @@ class DatasetAPIClient(BaseHttpClient):
         :param  params: The params to include in the GET request.
         :return: The response from the GET request.
         """
-
+        request_method = "GET"
+        self._log_request(request_method=request_method)
         response = self.get(
             self.full_url,
             params=params,
             headers=self.token_auth.get_auth_header(),
             verify=True,
         )
-        return response
+
+        return self._handle_response(response=response, request_method=request_method)
 
     def post_json(self, json_data: Dict) -> Response:
         """
@@ -43,17 +46,18 @@ class DatasetAPIClient(BaseHttpClient):
         :param json_data: The JSON data to include in the POST request.
         :return: The response from the POST request.
         """
+        request_method = "POST"
+        self._log_request(request_method=request_method, body=json_data)
         response = self.post(
             self.full_url,
             headers=self.token_auth.get_auth_header(),
             json=json_data,
             verify=True,
         )
-        if response.status_code != 201:
-            raise Exception(
-                f"POST request failed with status code: {response.status_code}"
-            )
-        return response
+
+        return self._handle_response(
+            response=response, request_method=request_method, body=json_data
+        )
 
     def put_json(self, json_data: Dict) -> Response:
         """
@@ -62,14 +66,58 @@ class DatasetAPIClient(BaseHttpClient):
         :param json_data: The JSON data to include in the PUT request.
         :return: The response from the PUT request.
         """
+        request_method = "PUT"
+        self._log_request(request_method=request_method, body=json_data)
+
         response = self.put(
             self.full_url,
             headers=self.token_auth.get_auth_header(),
             json=json_data,
             verify=True,
         )
-        if response.status_code != 200:
-            raise Exception(
-                f"PUT request failed with status code: {response.status_code}"
+
+        return self._handle_response(
+            response=response, request_method=request_method, body=json_data
+        )
+
+    def _handle_response(
+        self, response: Response, request_method: str, body: Optional[Dict] = None
+    ) -> Response:
+        data = self._get_log_data(
+            request_method=request_method, body=body, response=response.content
+        )
+        try:
+            logger.debug(
+                f"Received response code {response.status_code}",
+                data=data,
+                response=response,
             )
-        return response
+            response.raise_for_status()
+            return response
+        except HTTPError as err:
+            logger.error(
+                f"{request_method} failed", data=data, error=err, response=response
+            )
+            raise Exception(
+                f"{request_method} failed with status code: {response.status_code}"
+            ) from err
+
+    def _log_request(self, request_method: str, body: Optional[Dict] = None):
+        data = self._get_log_data(request_method=request_method, body=body)
+        logger.debug(f"Sending {request_method} request", data=data)
+
+    def _get_log_data(
+        self,
+        request_method: str,
+        response: Optional[str] = None,
+        body: Optional[dict] = None,
+    ) -> dict:
+        data = {"method": request_method, "url": self.full_url}
+
+        if response is not None:
+            data["response"] = response
+
+        if body is not None:
+            data["json"] = body
+
+        return data
--- dpytools/logging/__init__.py
+++ dpytools/logging/__init__.py
@@ -1,7 +1,4 @@
 from .data_exception import DataException
 from .logger import DpLogger
 
-all = [
-    DataException,
-    DpLogger
-]
+all = [DataException, DpLogger]
